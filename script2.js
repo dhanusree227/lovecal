a479306106b1f46d68e87e3e81584697cfe98128
dhanusree227 <dhanusreer2005@gmail.com>
2025-05-28 15:40:09 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,32 +1,25 @@
-// script.js
+document.getElementById('loveForm').addEventListener('submit',function(e){
+    e.preventDefault();
+    const yourName = document.getElementById('yourName').value.trim();
+    const crushName = document.getElementById('crushName').value.trim();
+    const resultDiv= document.getElementById('result');
 
-document.getElementById("loveForm").addEventListener("submit", function (e) {
-  e.preventDefault(); 
+    if(!yourName || !crushName) {
+        alert('Please fill the box with names');
+        return;
+    }
 
-  const yourName = document.getElementById("yourName").value.trim();
-  const crushName = document.getElementById("crushName").value.trim();
-  const resultDiv = document.getElementById("result");
+    const lovepercent =Math.floor(Math.random() *100);
 
+    let message = '';
+    if(lovepercent >= 90){
+        message = 'Match made in heaven!!';
+    }else if (lovepercent >=75){
+        message = 'It may work ,Try little Hard!!';
+    }else {
+        message = 'you got saved, enjoy your life!!';
+    }
 
-  if (yourName || crushName) {
-   alert("please fill in your names");
-    return;
-  }
-
-  
-  const loveScore = Math.floor(Math.random() * 100); 
-
-  let message = "";
-
-  if (loveScore >= 90) {
-    message ="a match made in heaven";
-  } else if (loveScore >= 75) {
-    message ="there is definitely something";
-  } 
-   else {
-    message = "you got saved,enjoy your life";
-  }
-
-  resultDiv.classList.remove("d-none");
-  resultDiv.innerHTML = ` ${yourname} & ${crushname} =${lovepercent}% - ${message}`;
-});
+    resultDiv.classList.remove('d-none');
+    resultDiv.innerHTML =`${yourName} & ${crushName} =${lovepercent}% - ${message}`;
+})
